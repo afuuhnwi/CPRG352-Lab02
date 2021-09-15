c9a5f576d4f88c93115bc7e864d3a8d02f16b8a4
794974 <794974@SAIT224920IT.ACDM.DS.SAIT.CA>
2021-09-15 14:30:13 -0600

    New Project Experimenting github

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,8 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String text = "Hello World!";
     }
     
 }
